493cab1ce6678f99087cde84a80b24b5d170e880
julien r <julien.riviere94120@gmail.com>
2021-12-22 17:21:03 +0100

    ajout de quelque commentaire

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.use((req, res) => { //réponse du server avec use est pour tous les types de
     next();
 });
 
+app.use((req, res, next) => {   //pour gérer les autorisation, a adapter
+    res.setHeader('Access-Control-Allow-Origin', '*');  //permet a n'importe quel source d'emmetre une requete
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //ajoute les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');    //envoies des requêtes avec les méthodes mentionnées
+    next();
+  });
+
 app.use((req, res) => {
     console.log("reponse envoye avec succes");
 });
